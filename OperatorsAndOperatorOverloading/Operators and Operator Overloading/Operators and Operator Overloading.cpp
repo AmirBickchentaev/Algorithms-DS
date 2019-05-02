1ca8a2fc6fbc696a59848f47f985ca52c39511ad
AmirBickchentaev <panasonic-5670@yandex.ru>
2019-05-02 16:56:09 +0300

    OperatorsAndOperatorOverloading

---
+++ OperatorsAndOperatorOverloading/Operators and Operator Overloading/Operators and Operator Overloading.cpp
@@ -0,0 +1,72 @@
+﻿// Operators and Operator Overloading.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+
+struct Vector 
+{
+	double x, y;
+
+	Vector(double x, double y) :x(x), y(y) {}
+	Vector Add(const Vector& other) const 
+	{
+		return Vector(x + other.x, y + other.y);
+	}
+	Vector Multiply(const Vector& other) const
+	{
+		return Vector(x * other.x, y * other.y);
+	}
+
+
+	Vector operator+(const Vector&other) const 
+	{
+		//implementaion using subFuction Add above
+		
+		//return Add(other);
+
+		return Vector(x + other.x, y + other.y);
+
+	}
+
+	Vector operator*(const Vector&other) const 
+	{
+		//implementation using subFunction Multiply above
+		//return Multiply(other); 
+
+		return Vector(x* other.x, y*other.y);
+	}
+
+
+	bool operator ==(const Vector&other) const 
+	{
+		return x == other.x && y == other.y;
+	}
+
+	bool operator!=(const Vector&other) const 
+	{
+		return !operator ==(other);
+		//`return !(*this == other);
+	}
+};
+
+
+ostream& operator<<(ostream& stream, const Vector&other) 
+{
+	stream << other.x << " , " << other.y;
+	return stream;
+}
+
+
+int main()
+{
+	Vector position(4.0, 5.0);
+	Vector speed(3.0, 1.0);
+	Vector result = position;
+
+
+	cout << position;
+
+}
