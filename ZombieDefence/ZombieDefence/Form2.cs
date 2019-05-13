9361fd459c1bd14aff7833ce1598873d3d5c3b23
AmirBickchentaev <panasonic-5670@yandex.ru>
2019-05-13 21:29:36 +0300

    ZombieDefence

---
+++ ZombieDefence/ZombieDefence/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZombieDefence
+{
+    public partial class Form2 : Form
+    {
+        SoundPlayer start = new SoundPlayer(Directory.GetCurrentDirectory() + @"\startMIs.wav");
+        public Form2()
+        {
+            start.Play();
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            this.Hide();
+            form.Show();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Form1 form = new Form1();
+                this.Hide();
+                form.Show();
+            }
+        }
+    }
+}
